8af6bf3990dca2cc0f32f818c23bd12805fe32e9
Nick Cao <nickcao@nichi.co>
2025-08-28 15:40:17 -0400

    Fix image

--- speech2text/reference_mlperf_perf.sh
+++ speech2text/reference_mlperf_perf.sh
@@ -23,7 +23,7 @@
 
 echo "Time Start: $(date +%s)"
 export WORKSPACE_DIR="/workspace"
-export DATA_DIR="/workspace/whisper/dataset"
+export DATA_DIR="/data"
 export MANIFEST_FILE="${DATA_DIR}/dev-all-repack.json"
 export RUN_LOGS=${WORKSPACE_DIR}/run_output
 export SCENARIO="Offline"
